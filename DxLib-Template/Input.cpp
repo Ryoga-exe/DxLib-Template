9dda0f76fdc405a3d6e1e740b0d4c12c3e64a488
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-08-08 15:04:40 +0900

    Delete Input.cpp

--- DxLib-Template/Input.cpp
+++ DxLib-Template/Input.cpp
@@ -1,7 +1,5 @@
 #include "Input.h"
 
 bool input::Update(bool drawKeyCodeFlag) {
-    Keyboard::Inst()->Update(drawKeyCodeFlag);
-    Mouse::Inst()->Update();
-    return false;
+    
 }
--- DxLib-Template/Input.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Keyboard/Keyboard.h"
-#include "Mouse/Mouse.h"
-
-namespace input {
-    bool Update(bool drawKeyCodeFlag = false);
-}
